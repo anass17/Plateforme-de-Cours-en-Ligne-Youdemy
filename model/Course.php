a8cd9cfdba9f15d0da27af34eccefcae96ade2b2
anass17 <anassboutaib2018@gmail.com>
2025-01-15 20:58:03 +0100

    Added Class Course

---
+++ model/Course.php
@@ -0,0 +1,25 @@
+<?php
+
+    abstract class Course {
+        protected int $course_id;
+        protected string $title;
+        protected string $description;
+        protected string $image_path;
+        protected string $publish_date;
+        protected string $type;
+        protected string $file_path;
+
+        public function __construct(int $course_id = 0, string $course_title = '', string $description = '', string $image_path = '', string $publish_date = '', string $type = '', string $file_path = '') {
+            $this -> course_id = $course_id;
+            $this -> course_title = $course_title;
+            $this -> description = $description;
+            $this -> image_path = $image_path;
+            $this -> publish_date = $publish_date;
+            $this -> type = $type;
+            $this -> file_path = $file_path;
+        }
+
+
+    }
+
+?>

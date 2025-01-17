991b05f573b98f6fb3b8860bf06154470fad92c6
anass17 <anassboutaib2018@gmail.com>
2025-01-17 17:19:15 +0100

    Added DocumentCourse Class

---
+++ classes/DocumentCourse.php
@@ -0,0 +1,15 @@
+<?php
+
+    // ------------------------------------
+    // Methods
+    // ------------------------------------
+
+    class DocumentCourse extends Course {
+        
+        public function uploadFile(array $file) : bool {
+
+            $allowed_types = ["application/pdf"];
+
+        }
+
+    }

18cd4052478648272f5bf2a3d8a187a2ea3618ea
anass17 <anassboutaib2018@gmail.com>
2025-01-17 23:34:07 +0100

    Added Display My Courses functionality

--- pages/teacher/my-courses.php
+++ pages/teacher/my-courses.php
@@ -3,10 +3,36 @@
 
     require __DIR__ . '/../../classes/Database.php';
     require __DIR__ . '/../../classes/Security.php';
+    require __DIR__ . '/../../classes/Category.php';
+    require __DIR__ . '/../../classes/User.php';
+    require __DIR__ . '/../../classes/Teacher.php';
+    require __DIR__ . '/../../classes/Course.php';
+    require __DIR__ . '/../../classes/DocumentCourse.php';
+    require __DIR__ . '/../../classes/VideoCourse.php';
+    require __DIR__ . '/../../classes/Helpers.php';
 
     $role = "";
 
     Security::createCSRFToken();
+
+    // getMyCourses
+
+    $user_row = Security::isAccessTokenValid();
+
+    if (!$user_row) {
+        header('Location: /pages/login.php');
+        exit;
+    }
+
+    $teacher = new Teacher($user_row['user_id']);
+
+    $my_courses = $teacher -> getMyCourses();
+
+    echo '<pre>';
+    print_r($my_courses);
+    echo '</pre>';
+
+    $categories = Category::getAllCategories();
 
 ?>
 
@@ -58,38 +84,42 @@
 
             <div class="mb-10">
                 <div class="mt-5 grid grid-cols-3 gap-5">
-                    <a class="bg-[#F0F0F0] rounded-lg border shadow-lg block" href="#">
-                        <div class="h-48 rounded-lg bg-[url('/assets/images/course-bg.jpg')] bg-cover bg-center">
 
+                    <!-- Courses -->
+
+                    <?php foreach($my_courses as $course): ?>
+                    
+                    <a class="bg-[#F0F0F0] rounded-lg border shadow-lg block group" href="<?php echo "/pages/teacher/view.php?id={$course -> getCourseId()}"; ?>">
+                        <div class="h-48 rounded-lg bg-[url('/assets/images/course-bg.jpg')] bg-cover bg-center relative" style='background-image: url("<?php echo $course -> getImagePath();?>")'>
+                            <div class="absolute top-3 left-3 bg-white group-hover:bg-opacity-100 transition-colors bg-opacity-65 w-6 h-6 rounded flex items-center justify-center">
+                                <?php if ($course -> getType() == "Video"): ?>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-gray-800" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c7.6-4.2 16.8-4.1 24.3 .5l144 88c7.1 4.4 11.5 12.1 11.5 20.5s-4.4 16.1-11.5 20.5l-144 88c-7.4 4.5-16.7 4.7-24.3 .5s-12.3-12.2-12.3-20.9l0-176c0-8.7 4.7-16.7 12.3-20.9z"/></svg>
+                                <?php elseif ($course -> getType() == "Document"): ?>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-gray-800" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M64 464l48 0 0 48-48 0c-35.3 0-64-28.7-64-64L0 64C0 28.7 28.7 0 64 0L229.5 0c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3L384 304l-48 0 0-144-80 0c-17.7 0-32-14.3-32-32l0-80L64 48c-8.8 0-16 7.2-16 16l0 384c0 8.8 7.2 16 16 16zM176 352l32 0c30.9 0 56 25.1 56 56s-25.1 56-56 56l-16 0 0 32c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-48 0-80c0-8.8 7.2-16 16-16zm32 80c13.3 0 24-10.7 24-24s-10.7-24-24-24l-16 0 0 48 16 0zm96-80l32 0c26.5 0 48 21.5 48 48l0 64c0 26.5-21.5 48-48 48l-32 0c-8.8 0-16-7.2-16-16l0-128c0-8.8 7.2-16 16-16zm32 128c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-16 0 0 96 16 0zm80-112c0-8.8 7.2-16 16-16l48 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-32 0 0 32 32 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-32 0 0 48c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-64 0-64z"/></svg>
+                                <?php endif; ?>
+                            </div>
                         </div>
-                        <div class="px-6 py-7 text-center">
-                            <h3 class="font-semibold text-[#00A5CF] text-lg">Learn Javascript Basics</h3>
+                        <div class="px-5 py-6 text-center">
+                            <h3 class="font-semibold text-[#0681a0] text-lg"><?php echo $course -> getTitle(); ?></h3>
                             <div class="flex items-center gap-2 text-[#555555] justify-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40L64 64C28.7 64 0 92.7 0 128l0 16 0 48L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-256 0-48 0-16c0-35.3-28.7-64-64-64l-40 0 0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40L152 64l0-40zM48 192l352 0 0 256c0 8.8-7.2 16-16 16L64 464c-8.8 0-16-7.2-16-16l0-256z"/></svg>
-                                <span class="text-sm">15 Jul 2024</span>
+                                <span class="text-sm"><?php echo Helpers::format_date($course -> getPublishDate()); ?></span>
                             </div>
                             <div class="mt-5 flex justify-between items-center">
-                                <h4 class="font-medium text-[#6F6F6F]">Anass Boutaib</h4>
-                                <span class="text-sm text-[#424242]">15 Subscriptions</span>
+                                <h4 class="font-medium text-[#6F6F6F] flex items-center gap-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-gray-600" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M264.5 5.2c14.9-6.9 32.1-6.9 47 0l218.6 101c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 149.8C37.4 145.8 32 137.3 32 128s5.4-17.9 13.9-21.8L264.5 5.2zM476.9 209.6l53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 277.8C37.4 273.8 32 265.3 32 256s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0l152-70.2zm-152 198.2l152-70.2 53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 405.8C37.4 401.8 32 393.3 32 384s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0z"/></svg>
+                                    <span class=""><?php echo $course -> getCategory() -> getCategoryName(); ?></span>
+                                </h4>
+                                <span class="text-sm text-[#424242] flex items-center gap-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-gray-600" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M219.3 .5c3.1-.6 6.3-.6 9.4 0l200 40C439.9 42.7 448 52.6 448 64s-8.1 21.3-19.3 23.5L352 102.9l0 57.1c0 70.7-57.3 128-128 128s-128-57.3-128-128l0-57.1L48 93.3l0 65.1 15.7 78.4c.9 4.7-.3 9.6-3.3 13.3s-7.6 5.9-12.4 5.9l-32 0c-4.8 0-9.3-2.1-12.4-5.9s-4.3-8.6-3.3-13.3L16 158.4l0-71.8C6.5 83.3 0 74.3 0 64C0 52.6 8.1 42.7 19.3 40.5l200-40zM111.9 327.7c10.5-3.4 21.8 .4 29.4 8.5l71 75.5c6.3 6.7 17 6.7 23.3 0l71-75.5c7.6-8.1 18.9-11.9 29.4-8.5C401 348.6 448 409.4 448 481.3c0 17-13.8 30.7-30.7 30.7L30.7 512C13.8 512 0 498.2 0 481.3c0-71.9 47-132.7 111.9-153.6z"/></svg>
+                                    <span>15</span>
+                                </span>
                             </div>
                         </div>
                     </a>
-                    <a class="bg-[#F0F0F0] rounded-lg border shadow-lg block" href="#">
-                        <div class="h-48 rounded-lg bg-[url('/assets/images/course-bg.jpg')] bg-cover bg-center">
 
-                        </div>
-                        <div class="px-5 py-5 text-center">
-                            <h3 class="font-semibold text-[#00A5CF] text-lg">Learn Javascript Basics</h3>
-                            <div class="flex items-center gap-2 text-[#555555] justify-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40L64 64C28.7 64 0 92.7 0 128l0 16 0 48L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-256 0-48 0-16c0-35.3-28.7-64-64-64l-40 0 0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40L152 64l0-40zM48 192l352 0 0 256c0 8.8-7.2 16-16 16L64 464c-8.8 0-16-7.2-16-16l0-256z"/></svg>
-                                <span class="text-sm">15 Jul 2024</span>
-                            </div>
-                            <div class="mt-5 flex justify-between items-center">
-                                <h4 class="font-medium text-[#6F6F6F]">Anass Boutaib</h4>
-                                <span class="text-sm text-[#424242]">15 Subscriptions</span>
-                            </div>
-                        </div>
-                    </a>
+                    <?php endforeach; ?>
+
                 </div>
             </div>
 
@@ -137,8 +167,11 @@
                         <label for="form-course-category" class="block mb-2">Category *</label>
                         <select name="course-category" id="form-course-category" class="w-full px-4 py-2 rounded border outline-none border-blue-200 bg-gray-100">
                             <option value="">Select a category</option>
-                            <option value="1">Technology</option>
-                            <option value="2">Sports</option>
+                            <?php foreach($categories as $category): ?>
+
+                                <?php echo "<option value='{$category -> getCategoryId()}'>{$category -> getCategoryName()}</option>"; ?>
+
+                            <?php endforeach; ?>
                         </select>
                         <small class="text-red-400 font-semibold"></small>
                     </div>

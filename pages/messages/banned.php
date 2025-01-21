9bf5f2c9dcd3b784a3e86853439aa93cf3be1477
anass17 <anassboutaib2018@gmail.com>
2025-01-21 01:52:19 +0100

    Added pages for pending and banned messages

---
+++ pages/messages/banned.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banned</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playwrite+IN:wght@100..400&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+    <div class="h-screen flex justify-center items-center flex-col">
+        <h1 class="text-gray-700 text-3xl font-semibold mb-6">Access Denied</h1>
+
+        <p class="max-w-3xl text-center mb-8 text-gray-500">We're sorry, but your account has been suspended due to a violation of our community guidelines or terms of service. If you believe this is a mistake, please contact us at <span class="text-[#00A5CF] font-semibold">contact@youdemy.com</span></p>
+
+        <div class="flex gap-3">
+            <a href="/pages/logout.php" class="w-32 h-11 rounded bg-[#00A5CF] text-white flex justify-center items-center">Logout</a>
+        </div>
+    </div>
+
+
+</body>
+</html>
---
+++ pages/messages/pending.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pending</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playwrite+IN:wght@100..400&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+    <div class="h-screen flex justify-center items-center flex-col">
+        <h1 class="text-gray-700 text-3xl font-semibold mb-6">Thank you for registering!</h1>
+
+        <p class="max-w-3xl text-center mb-8 text-gray-500">Your account has been successfully created. We're reviewing your registration and will notify you once it has been approved by our admin team. We appreciate your patience and look forward to having you join our community soon!</p>
+
+        <div class="flex gap-3">
+            <a href="/pages/logout.php" class="w-32 h-11 rounded bg-[#00A5CF] text-white flex justify-center items-center">Logout</a>
+        </div>
+    </div>
+
+
+</body>
+</html>
